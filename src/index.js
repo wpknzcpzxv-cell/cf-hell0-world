c1d457303fa57bc4e23c6a8bcdd40bcf83f08103
wpknzcpzxv-cell <wpknzcpzxv@privaterelay.appleid.com>
2025-10-25 13:12:44 +0300

    chore: log fetch invocations for observability

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
+    console.log("[FETCH] cf-hell0-world got a request at " + new Date().toISOString());
     return new Response("Hello from cf-hell0-world 👁", {
       status: 200,
       headers: { "content-type": "text/plain; charset=utf-8" },
